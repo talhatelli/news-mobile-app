c2fa2adf673425e1f146407a063e053284200faa
talhatelli <ahmet.telli@tsoft.club>
2024-11-22 14:54:46 +0300

    push notification setup provided

--- src/App.tsx
+++ src/App.tsx
@@ -13,78 +13,50 @@ import { IonReactRouter } from "@ionic/react-router";
 import { newspaperOutline, statsChartOutline } from "ionicons/icons";
 import News from "./pages/News";
 import Chart from "./pages/Chart";
-import { PushNotifications } from '@capacitor/push-notifications';
-import { useEffect } from 'react';
-/* Core CSS required for Ionic components to work properly */
+import { PushNotifications } from "@capacitor/push-notifications";
+import { useEffect } from "react";
 import "@ionic/react/css/core.css";
-
-/* Basic CSS for apps built with Ionic */
 import "@ionic/react/css/normalize.css";
 import "@ionic/react/css/structure.css";
 import "@ionic/react/css/typography.css";
-
-/* Optional CSS utils that can be commented out */
 import "@ionic/react/css/padding.css";
 import "@ionic/react/css/float-elements.css";
 import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-/*import "@ionic/react/css/palettes/dark.system.css";
-
-/* Theme variables */
 import "./theme/variables.css";
 
 setupIonicReact();
 
 const App: React.FC = () => {
-
   useEffect(() => {
-    // Request permission to use push notifications
-    // iOS will prompt user and return if they granted permission or not
-    // Android will just grant without prompting
-    PushNotifications.requestPermissions().then(result => {
-      if (result.receive === 'granted') {
+    PushNotifications.requestPermissions().then((result) => {
+      if (result.receive === "granted") {
         // Register with Apple / Google to receive push via APNS/FCM
         PushNotifications.register();
       }
     });
 
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration',
-      (token) => {
-        console.log('Push registration success, token: ' + token.value);
-      }
-    );
+    PushNotifications.addListener("registration", (token) => {
+      console.log("Push registration success, token: " + token.value);
+    });
 
-    // Some issue with our setup and push will not work
-    PushNotifications.addListener('registrationError',
-      (error) => {
-        console.log('Error on registration: ' + JSON.stringify(error));
-      }
-    );
+    PushNotifications.addListener("registrationError", (error) => {
+      console.log("Error on registration: " + JSON.stringify(error));
+    });
 
-    // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived',
+    PushNotifications.addListener(
+      "pushNotificationReceived",
       (notification) => {
-        console.log('Push received: ' + JSON.stringify(notification));
+        console.log("Push received: " + JSON.stringify(notification));
       }
     );
 
-    // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed',
+    PushNotifications.addListener(
+      "pushNotificationActionPerformed",
       (notification) => {
-        console.log('Push action performed: ' + JSON.stringify(notification));
+        console.log("Push action performed: " + JSON.stringify(notification));
       }
     );
   }, []);
